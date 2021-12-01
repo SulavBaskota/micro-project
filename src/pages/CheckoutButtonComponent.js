fbc620c931ae06badaa51502b78352fe95b4b26d
Sulav Baskota <sulav.baskota0419@gmail.com>
2021-12-01 21:28:07 +0545

    Checkout button component created. Need to create event handler for the Checkout button.

---
+++ src/pages/CheckoutButtonComponent.js
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { Box, Button } from '@mui/material';
+import { Fragment } from 'react';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+export default function CheckoutButton({ items, total }) {
+    return (
+        <Fragment>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    p: 0,
+                    m: 0
+                }}
+            >
+                <Box
+                    sx={{
+                        boxShadow: 8,
+                        width: '95%',
+                        height: 100,
+                        borderRadius: 1.5,
+                        position: 'Fixed',
+                        bottom: 0,
+                        m: 0.5,
+                        backgroundColor: 'white',
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Button
+                        variant="contained"
+                        size="large"
+                        color="success"
+                        sx={{
+                            ml: 1,
+                            mt: 1,
+                            mr: 1,
+                            width: '98%',
+                            height: 50,
+                            textTransform: 'none'
+                        }}
+                    >
+                        <Grid container spacing={1}>
+                            <Grid item xs={3}>
+                                <Typography variant="body1" component="div" align="left">
+                                    {items}{items === 1 ? ' Item' : ' Items'}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Typography variant="body1" component="div">
+                                    CHECKOUT
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Typography variant="body1" component="div" align="right">
+                                    ${total}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Button>
+                </Box>
+            </Box>
+        </Fragment>
+    );
+}
--- src/pages/MenuComponent.js
+++ src/pages/MenuComponent.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Fragment, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -10,86 +12,98 @@ import RemoveIcon from '@mui/icons-material/Remove';
 import Paper from '@mui/material/Paper';
 import { Divider } from '@mui/material';
 import { MenuList } from './MenuItems';
-import { Fragment } from 'react';
-import { useState } from 'react';
+import CheckoutButton from './CheckoutButtonComponent';
 
 export default function Menu() {
+    let { id } = useParams();
     const [order, setOrder] = useState({});
+    const [items, setItems] = useState(0);
+    const [total, setTotal] = useState(0);
 
-    const handleAddItem = (id) => {
-        if (order && (id in order)) {
+    const handleAddItem = (itemId, itemPrice) => {
+        if (itemId in order) {
             setOrder({
                 ...order,
-                [id]: order[id] + 1,
+                [itemId]: order[itemId] + 1,
             });
         } else {
             setOrder({
                 ...order,
-                [id]: 1,
+                [itemId]: 1,
             });
-        }   
+        }
+        setItems(items + 1);
+        setTotal(total + itemPrice);
     }
 
-    const handleRemoveItem = (id) => {
-        if (order && (id in order)) {
-            if (order[id] > 1) {
+    const handleRemoveItem = (itemId, itemPrice) => {
+        if (itemId in order) {
+            if (order[itemId] > 1) {
                 setOrder({
                     ...order,
-                    [id]: order[id] - 1,
+                    [itemId]: order[itemId] - 1,
                 })
             } else {
                 let newOrder = { ...order };
-                delete newOrder[id];
+                delete newOrder[itemId];
                 setOrder(newOrder);
             }
+            setItems(items - 1);
+            setTotal(total - itemPrice);
         }
     }
 
     return (
         <Fragment>
             {console.log(order)}
-            {MenuList.map((category, idx) => (
-                <Box key={idx} sx={{ m: 2 }}>
-                    <Typography component="h1" variant="h4">
-                        {category.category}
-                    </Typography>
-                    <Divider variant="middle" sx={{ mb: 1 }} />
-                    {category.items.map((item, index) => (
-                        <Card sx={{ display: 'flex', mb: 2 }} key={index}>
-                            <CardMedia
-                                component="img"
-                                sx={{ width: 151 }}
-                                image={item.img}
-                                alt={item.img_alt}
-                            />
-                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                                <CardContent sx={{ flex: '1 0 auto' }}>
-                                    <Typography component="div" variant="h6">
-                                        {item.name}
-                                    </Typography>
-                                    <Typography variant="subtitle1" color="text.secondary" component="div">
-                                        ${item.price}
-                                    </Typography>
-                                </CardContent>
-                                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-                                    <IconButton aria-label="remove" color="error" onClick={() => handleRemoveItem(item.id)}>
-                                        <RemoveIcon />
-                                    </IconButton>
-                                    <Paper elevation={0} variant="outlined" sx={{ width: 40 }}>
-                                        <Typography component="div" variant="h5" align="center">
-                                            {order && (item.id in order) ? order[item.id] : 0}
+            <Box sx={{ pb: 15 }}>
+                {MenuList.map((category, idx) => (
+                    <Box key={idx} sx={{ m: 2 }}>
+                        <Typography component="h1" variant="h4">
+                            {category.category}
+                        </Typography>
+                        <Divider variant="middle" sx={{ mb: 1 }} />
+                        {category.items.map((item, index) => (
+                            <Card sx={{ display: 'flex', mb: 2 }} key={index}>
+                                <CardMedia
+                                    component="img"
+                                    sx={{ width: 151 }}
+                                    image={item.img}
+                                    alt={item.img_alt}
+                                />
+                                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                    <CardContent sx={{ flex: '1 0 auto' }}>
+                                        <Typography component="div" variant="h6">
+                                            {item.name}
                                         </Typography>
-                                    </Paper>
-                                    <IconButton aria-label="add" color="success" onClick={() => handleAddItem(item.id)}>
-                                        <AddIcon />
-                                    </IconButton>
+                                        <Typography variant="subtitle1" color="text.secondary" component="div">
+                                            ${item.price}
+                                        </Typography>
+                                    </CardContent>
+                                    <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                                        <IconButton aria-label="remove" color="error" onClick={() => handleRemoveItem(item.id, item.price)}>
+                                            <RemoveIcon />
+                                        </IconButton>
+                                        <Paper elevation={0} variant="outlined" sx={{ width: 40 }}>
+                                            <Typography component="div" variant="h5" align="center">
+                                                {order && (item.id in order) ? order[item.id] : 0}
+                                            </Typography>
+                                        </Paper>
+                                        <IconButton aria-label="add" color="success" onClick={() => handleAddItem(item.id, item.price)}>
+                                            <AddIcon />
+                                        </IconButton>
+                                    </Box>
                                 </Box>
-                            </Box>
-
-                        </Card>
-                    ))}
-                </Box>
-            ))}
-        </Fragment>
+                            </Card>
+                        ))}
+                    </Box>
+                ))}
+            </Box>
+            {Object.keys(order).length !== 0 ?
+                (
+                    <CheckoutButton items={items} total={total} />
+                ) : null
+            }
+        </Fragment >
     );
 }
